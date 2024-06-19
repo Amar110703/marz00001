9733d9ec6cbc034b33781646e8f10a07f0f94f21
Amar110703 <utolu5484@gmail.com>
2024-06-20 01:11:47 +0700

    Update instal_rdp.sh

--- instal_rdp.sh
+++ instal_rdp.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Pastikan script dijalankan sebagai root atau dengan akses sudo
-if [ "$(id -u)" -ne 0 ]; then
-  echo "Script harus dijalankan sebagai root atau dengan sudo."
-  exit 1
-fi
+echo "Masukan ip anda :"
+read ip
 
-# Update repository dan instal dependencies
-apt update
-apt install -y xrdp xfce4 xfce4-goodies
+echo "Masukan domain anda :"
+read domain
 
-# Konfigurasi sesuai kebutuhan
-echo "xfce4-session" > ~/.xsession
+echo "Masukan node domain anda :"
+read node
 
-# Mulai layanan xrdp
-systemctl enable xrdp
-systemctl start xrdp
-
-# Konfigurasi firewall untuk mengizinkan koneksi RDP (port 3389)
-ufw allow 3389/tcp
-
-# Restart layanan xrdp
-systemctl restart xrdp
-
-echo "Instalasi RDP selesai. Silakan koneksikan menggunakan klien RDP ke IP VPS Anda."
+echo "Halo, $ip! Selamat datang di script shell ini."
+echo "$domain"
+echo "$node"
+echo -e "
+"
+bash <(curl -s https://pterodactyl-installer.se)
