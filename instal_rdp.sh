255784b8cdd0c78005a7484ae14cde05db1bf939
Amar110703 <utolu5484@gmail.com>
2024-06-20 02:30:47 +0700

    Update instal_rdp.sh

--- instal_rdp.sh
+++ instal_rdp.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-
-echo "Masukan ip anda :"
-read ip
-
-echo "Masukan domain anda :"
-read domain
-
-echo "Masukan node domain anda :"
-read node
-
-echo "Masukan username untuk panel :"
-read user
-
-echo "Masukan password untuk panel :"
+echo "password"
 read pw
 
-curl -s https://pterodactyl-installer.se | bash -s -- << EOF
+
+# Menjalankan installer Pterodactyl dengan curl dan heredoc untuk mengirimkan input
+curl -s https://pterodactyl-installer.se | bash -s -- <<EOF
 0
+$pw
 EOF
 
 echo "Instalasi selesai."
