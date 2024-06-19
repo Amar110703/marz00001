63e578c403a49db32adb36ce9d417864f1b606d5
Amar110703 <utolu5484@gmail.com>
2024-06-20 01:22:02 +0700

    Update instal_rdp.sh

--- instal_rdp.sh
+++ instal_rdp.sh
@@ -9,9 +9,36 @@ read domain
 echo "Masukan node domain anda :"
 read node
 
+echo "Masukan username untuk panel :"
+read user
+
+echo "Masukan password untuk panel :"
+read pw
+
 echo "Halo, $ip! Selamat datang di script shell ini."
 echo "$domain"
 echo "$node"
+echo "$user"
+echo "$pw"
 echo -e "
 "
 bash <(curl -s https://pterodactyl-installer.se)
+0
+admin
+admin
+1
+Asia/Jakarta
+admin@gmail.com
+admin@gmail.com
+$user
+$user
+$user
+$pw
+$domain
+y
+y
+y
+y
+y
+yes
+a
