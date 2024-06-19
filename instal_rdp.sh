d3f37ddbeb5bd029a061fac7afbf5748e06bc66a
Amar110703 <utolu5484@gmail.com>
2024-06-20 02:36:35 +0700

    Update instal_rdp.sh

--- instal_rdp.sh
+++ instal_rdp.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-echo "password"
-read pw
 
+# Definisikan variabel yang diperlukan
+pw="password"
+domain="example.com"
+ip="192.168.1.100"
+node="node1"
 
 # Menjalankan installer Pterodactyl dengan curl dan heredoc untuk mengirimkan input
 curl -s https://pterodactyl-installer.se | bash -s -- <<EOF
-0
 $pw
+$pw
+$domain
+$ip
+$node
 EOF
 
 echo "Instalasi selesai."
