cea0a692121c6aff2b2070895a7e795dd4ea9f45
Amar110703 <utolu5484@gmail.com>
2024-06-20 14:57:08 +0700

    Update and rename instal_rdp.sh to instal.sh

---
+++ instal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Masukan ip anda :"
+read ip
+
+echo "Masukan domain anda :"
+read domain
+
+echo "Masukan node domain anda :"
+read node
+
+echo "Masukan user untuk login ke panel"
+read user
+
+echo "Masukan password untuk login ke panel"
+read pw
+
+echo "Halo, $user! Selamat datang di script shell ini."
+echo -e "
+"
+bash <(curl -s https://pterodactyl-installer.se)
+echo "0" | instal.sh
